17de5aa6fe06859d96aeb937748e9c53dd269f60
Ana Ghenciu <anamaria.ghenciu@gmail.com>
2015-02-03 16:34:26 +0000

    adding fileds

--- db/migrate/20150202190754_create_ideas.rb
+++ db/migrate/20150202190754_create_ideas.rb
@@ -2,6 +2,7 @@ class CreateIdeas < ActiveRecord::Migration
   def change
     create_table :ideas do |t|
       t.string :name
+      t.string :description
 
       t.timestamps null: false
     end
